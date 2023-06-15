be2aa7928cfb54a0ab22b9760c7b63ce73bca94c
matteo-didone <matteo.dido9@gmail.com>
2023-06-15 10:04:40 +0200

    Implementing corrections

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -1,6 +1,24 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppTitle,
+    AppHero
+  },
+
+  methods: {
+    getImagePath: function (imageName) {
+      return new URL(`../assets/${imageName}`, import.meta.url).href;
+    }
+  }
+}
+</script>
+
 <script setup>
-  import AppTitle from './components/AppTitle.vue'
-  import AppHero from './components/AppHero.vue'
+
 </script>
 
 <template>
@@ -8,6 +26,12 @@
   <AppHero />
 </template>
 
-<style scoped>
+<style scoped></style>
 
-</style>
+
+<template>
+  <AppTitle msg="La mia prima app con Vite!" />
+  <AppHero />
+</template>
+
+<style scoped></style>
